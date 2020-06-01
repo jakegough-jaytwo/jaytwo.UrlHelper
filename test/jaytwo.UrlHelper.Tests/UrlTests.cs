fa1b40b0ef4eda25c4fec6dbf8b69e654494ccb6
Jake Gough <jakegough@gmail.com>
2020-06-01 10:06:16 -0600

    more format tests

--- test/jaytwo.UrlHelper.Tests/UrlTests.cs
+++ test/jaytwo.UrlHelper.Tests/UrlTests.cs
@@ -111,6 +111,7 @@ namespace jaytwo.UrlHelper.Tests
         [Theory]
         [InlineData("http://www.google.com/{0}/foo.bar", new[] { "banana" }, "http://www.google.com/banana/foo.bar")]
         [InlineData("http://www.google.com/{0}/foo.bar", new[] { default(string) }, "http://www.google.com//foo.bar")]
+        [InlineData("{0}/foo.bar", new object[] { "abc" }, "abc/foo.bar")]
         public void Format(string format, string[] args, string expectedUrl)
         {
             // arrange
@@ -125,6 +126,7 @@ namespace jaytwo.UrlHelper.Tests
         [Theory]
         [InlineData("http://www.google.com/{0}/foo.bar", new object[] { 1 }, "http://www.google.com/1/foo.bar")]
         [InlineData("http://www.google.com/{0}/foo.bar", new object[] { null }, "http://www.google.com//foo.bar")]
+        [InlineData("{0}/foo.bar", new object[] { "abc" }, "abc/foo.bar")]
         public void Format_value_object(string format, object[] args, string expectedUrl)
         {
             // arrange
